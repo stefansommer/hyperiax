734a11d1c4a8816f86a8288d4f53020c714efb81
Stefan Sommer <sommer@di.ku.dk>
2024-04-26 14:48:21 +0200

    product fusing

--- hyperiax/models/functional/reducers.py
+++ hyperiax/models/functional/reducers.py
@@ -10,3 +10,14 @@ def sum_fuse_children(axis=-1):
         return {k[6:]:v.sum(axis=axis) for k,v in kwargs.items() if k.startswith('child_')}
     return _fuse
 
+
+def product_fuse_children(axis=-1):
+    """returns a function that computes the product of an axis of keyed values prefixed with `child_`
+
+    Args:
+        axis (int, optional): the axis to compute product over. Defaults to -1.
+    """
+    def _fuse(**kwargs):
+        return {k[6:]:v.prod(axis=axis) for k,v in kwargs.items() if k.startswith('child_')}
+    return _fuse
+
