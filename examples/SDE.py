2372f345c33b5a31a4c5028bbdc27a76e0488e09
Stefan Sommer <sommer@di.ku.dk>
2024-05-27 22:37:30 +0200

    removed debug/test code

--- examples/SDE.py
+++ examples/SDE.py
@@ -37,17 +37,8 @@ def forward_guided(x,H_T,F_T,tildea,dts,dWs,b,sigma,params):
 
     T = jnp.sum(dts)
     Phi_inv = lambda t: jnp.eye(H_T.shape[0])+H_T@tildea*(T-t)
-    #Phi = lambda t: jnp.linalg.inv(Phi_inv(t))
-    #Phi_0 = Phi(0.); Phi_T = Phi(T)#jnp.eye(H[1].shape[0])
-    #Phi = lambda t: (T-t)/t*Phi_0+t/T*Phi_T
     Ht = lambda t: solve(Phi_inv(t),H_T).reshape(H_T.shape) 
     Ft = lambda t: solve(Phi_inv(t),F_T).reshape(F_T.shape) 
-    #Ht = lambda t: dot(Phi(t),H[1]).reshape(H[1].shape) 
-    #Ft = lambda t: dot(Phi(t),F[1]).reshape(F[1].shape) 
-    #Ht = lambda t: #(t*Phi_T+(T-t)*Phi_0)@H[1]
-    #Ft = lambda t: #dot(t*Phi_T+(T-t)*Phi_0,F[1])
-    #Ht = lambda t: (T-t)/T*H[0]-t/T*H[1]
-    #Ft = lambda t: (T-t)/T*F[0]-t/T*F[1]
 
     def bridge_SFvdM(carry, val):
         t, X, logpsi = carry
